fc46bbd0b2174cb994b2d1bd1eac2b176b9ff197
Isntitfun <112168128+Isntitfun@users.noreply.github.com>
2022-09-14 00:01:36 +0900

    #2.6 ~ #2.10

--- javascript/app.js
+++ javascript/app.js
@@ -8,6 +8,8 @@ alert("hi");
 "apple" + " killer" + 3;
 "apple" + " killer" + 3.5;
 
+// Console.log
+
 console.log(2 + 2);
 console.log(2 + 1.5);
 console.log(1.5 + 1.5);
@@ -16,6 +18,8 @@ console.log("apple killer");
 console.log("apple" + " killer");
 console.log("apple" + " killer" + 3);
 console.log("apple" + " killer" + 3.5);
+
+// Const / Let / Var
 
 const a = 10;
 const b = 2;
@@ -69,6 +73,8 @@ const v = "Never var";
 
 console.log(c + l + v);
 
+// Booleans
+
 const t = true;
 const f = false;
 const n = null;
@@ -81,6 +87,8 @@ console.log("TRUE = the Variable EXISTS and it is defined as TRUE");
 console.log("FALSE = the Variable EXISTS and it is defined as FALSE");
 console.log("NULL = the Variable EXISTS and it is defined as NOTHING");
 console.log("UNDEFINED = the Variable EXISTS but it is NOT DEFINED");
+
+// Array
 
 const weekday = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
 
@@ -118,3 +126,91 @@ const fruit = {
 
 console.log(fruit);
 console.log(fruit.type[0]);
+
+// function
+
+function sayHello() {
+  console.log("Hello my name is");
+}
+
+sayHello();
+sayHello();
+sayHello();
+
+function wink() {
+  console.log("^ 3 *");
+}
+
+wink();
+wink();
+wink();
+
+// function 2
+
+function sayHello(name, age) {
+  console.log("Helllo my name is " + name + " and I'm " + age);
+}
+
+sayHello("SH", 1);
+
+// making calculator using function
+
+function addition(x1, x2, x3) {
+  console.log(x1 + x2 + x3);
+}
+
+function subtraction(x1, x2, x3) {
+  console.log(x1 - x2 - x3);
+}
+
+function multiplication(x1, x2, x3) {
+  console.log(x1 * x2 * x3);
+}
+
+function division(x1, x2, x3) {
+  console.log(x1 / x2 / x3);
+}
+
+addition(1, 2, 3);
+subtraction(1, 2, 3);
+multiplication(1, 2, 3);
+division(1, 2, 3);
+
+// making object using function
+
+const elon = {
+  age: 31,
+  mf: "male",
+  home: "California",
+  bark: function (status) {
+    console.log("Elon : woof! wooooof! wof! woof!" + " (" + status + ")");
+  },
+};
+console.log(elon);
+
+console.log(elon.age);
+
+elon.bark("fiercely");
+elon.bark("mildly");
+
+// Example
+
+const calculator = {
+  add: function (x1, x2) {
+    console.log(x1 + x2);
+  },
+  subtract: function (x1, x2) {
+    console.log(x1 - x2);
+  },
+  divide: function (x1, x2) {
+    console.log(x1 / x2);
+  },
+  power: function (x1, x2) {
+    console.log(x1 ** x2);
+  },
+};
+
+calculator.add(2, 2);
+calculator.subtract(2, 2);
+calculator.divide(2, 2);
+calculator.power(2, 2);
