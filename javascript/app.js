fc033b5b5da582ce8458b0d52cad5f6162df342d
Isntitfun <112168128+Isntitfun@users.noreply.github.com>
2022-09-13 12:47:37 +0900

    #2.5~#2.6

--- javascript/app.js
+++ javascript/app.js
@@ -1,90 +1,120 @@
-alert("hi")
-2+2
-2+1.5
-1.5+1.5
-"apple"
-"apple killer"
-"apple"+" killer"
-"apple"+" killer"+3
-"apple"+" killer"+3.5
+alert("hi");
+2 + 2;
+2 + 1.5;
+1.5 + 1.5;
+("apple");
+("apple killer");
+"apple" + " killer";
+"apple" + " killer" + 3;
+"apple" + " killer" + 3.5;
 
-console.log(2+2)
-console.log(2+1.5)
-console.log(1.5+1.5)
-console.log("apple")
-console.log("apple killer")
-console.log("apple"+" killer")
-console.log("apple"+" killer"+3)
-console.log("apple"+" killer"+3.5)
+console.log(2 + 2);
+console.log(2 + 1.5);
+console.log(1.5 + 1.5);
+console.log("apple");
+console.log("apple killer");
+console.log("apple" + " killer");
+console.log("apple" + " killer" + 3);
+console.log("apple" + " killer" + 3.5);
 
-const a = 10
-const b = 2
-const text = "Hidden "
-const myName = "SH"
+const a = 10;
+const b = 2;
+const text = "Hidden ";
+const myName = "SH";
 
-console.log(a+b)
-console.log(a*b)
-console.log(a/b)
-console.log(text+myName)
+console.log(a + b);
+console.log(a * b);
+console.log(a / b);
+console.log(text + myName);
 
-console.log("LET")
+console.log("LET");
 
-let C = "Always const "
-let L = "Sometimes let "
-let V = "Never var"
+let C = "Always const ";
+let L = "Sometimes let ";
+let V = "Never var";
 
-console.log(C+L+V)
+console.log(C + L + V);
 
-C = "Never const "
-L = "Always or Never let "
-V = "Always var"
+C = "Never const ";
+L = "Always or Never let ";
+V = "Always var";
 
-console.log(C+L+V)
+console.log(C + L + V);
 
-console.log("VAR")
+console.log("VAR");
 
+var cs = "Always const ";
+var lt = "Sometimes let ";
+var vr = "Never var";
 
-var cs = "Always const "
-var lt = "Sometimes let "
-var vr = "Never var"
+console.log(cs + lt + vr);
 
-console.log(cs+lt+vr)
+cs = "Never const";
+lt = "Always or Never let";
+vr = "Always var";
 
-cs = "Never const"
-lt = "Always or Never let"
-vr = "Always var"
+console.log(cs + lt + vr);
 
-console.log(cs+lt+vr)
+var cs = "Never const";
+var lt = "Always or Never let";
+var vr = "Always var";
 
-var cs = "Never const"
-var lt = "Always or Never let"
-var vr = "Always var"
+console.log(cs + lt + vr);
 
-console.log(cs+lt+vr)
+console.log("CONST");
 
-console.log("CONST")
+const c = "Always const ";
+const l = "Sometimes let ";
+const v = "Never var";
 
-const c = "Always const "
-const l = "Sometimes let "
-const v = "Never var"
+console.log(c + l + v);
 
-console.log(c+l+v)
+const t = true;
+const f = false;
+const n = null;
+const u = console.log(t);
+console.log(f);
+console.log(n);
+console.log(u);
 
+console.log("TRUE = the Variable EXISTS and it is defined as TRUE");
+console.log("FALSE = the Variable EXISTS and it is defined as FALSE");
+console.log("NULL = the Variable EXISTS and it is defined as NOTHING");
+console.log("UNDEFINED = the Variable EXISTS but it is NOT DEFINED");
 
-const t = true
-const f = false
-const n = null
-const u =
+const weekday = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
 
-console.log(t)
-console.log(f)
-console.log(n)
-console.log(u)
+console.log(weekday[6]);
+console.log(weekday[7]);
 
-console.log("TRUE = the Variable EXISTS and it is defined as TRUE")
-console.log("FALSE = the Variable EXISTS and it is defined as FALSE")
-console.log("NULL = the Variable EXISTS and it is defined as NOTHING")
-console.log("UNDEFINED = the Variable EXISTS but it is NOT DEFINED")
+weekday.push("X");
 
+console.log(weekday[7]);
 
+// Object
 
+const user = {
+  name: "brian",
+  job: "tagger",
+  credibility: "96%",
+  balance: 130000,
+};
+
+console.log(user);
+console.log(user.name);
+
+user.balance = user.balance - 70000;
+
+user.identeActive = true;
+
+console.log(user);
+
+// object Experiment
+
+const fruit = {
+  type: ["apple", "lemon", "orange"],
+  color: "yellow",
+};
+
+console.log(fruit);
+console.log(fruit.type[0]);
