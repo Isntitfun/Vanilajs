a2ee6a4c5e6625099e4c127dfa8f4ea227fa74b3
Isntitfun <112168128+Isntitfun@users.noreply.github.com>
2022-09-14 23:35:39 +0900

    #2.13 ~ #3.1

---
+++ javascript/javascript on browser.js
@@ -0,0 +1,10 @@
+document.getElementById("title");
+const title = document.getElementById("title");
+
+console.log(title);
+
+console.dir(title);
+
+console.log(title.className);
+
+title.innerText = "brrr";
