3abc12bf309fd7a5e5e4dda75c257ab389f90568
Isntitfun <112168128+Isntitfun@users.noreply.github.com>
2022-09-18 14:56:17 +0900

    mid

--- javascript/app.js
+++ javascript/app.js
@@ -4,11 +4,15 @@ const loginInput = loginForm.querySelector("input")
 const HIDDEN_CLASSNAME = "hidden"
 const USERNAME = "username"
 
+function createGreeting(){
+  
+}
 
 function handleSubmitForm(event){
   event.preventDefault()
   loginForm.classList.add(HIDDEN_CLASSNAME)
   const username = loginInput.value
+  localStorage.setItem(USERNAME, username)
 }
 
 loginForm.addEventListener("submit", handleSubmitForm)
