7b5e551b187505c12c1b28ca4d68ca6a7d949016
Isntitfun <112168128+Isntitfun@users.noreply.github.com>
2022-09-20 23:56:05 +0900

    Demo

--- javascript/weather.js
+++ javascript/weather.js
@@ -4,8 +4,10 @@ function getGeoFail() {
 
 function getGeoOk(position) {
   const lat = position.coords.latitude;
-  const lng = position.coords.longitude;
-  console.log(`current geocoordinate is ${lat}, ${lng}`);
+  const lon = position.coords.longitude;
+  const API_KEY = "37d4fa6a5ec81e3393fee85fccdd6925";
+  const url = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude={part}&appid=${API_KEY}`;
+  fetch(url);
 }
 
 navigator.geolocation.getCurrentPosition(getGeoOk, getGeoFail);
